d5b4e29705b244ea55c640d52c258a186ceba438
doantrancaotri1108 <doantrancaotri1108@gmail.com>
2025-04-19 02:21:59 +0700

    change be address

--- src/lib/api/API-Endpoint.ts
+++ src/lib/api/API-Endpoint.ts
@@ -1,5 +1,5 @@
-const URL_PREFIX = "http://52.23.164.179:8000"; //import.meta.env.VITE_API_URL // 
-const WEBSOCKET_PREFIX = "ws://52.23.164.179:8000/ws/users";// import.meta.env.VITE_WEBSOCKET_URL 
+const URL_PREFIX = "http://3.89.90.31:8000"; //import.meta.env.VITE_API_URL // 
+const WEBSOCKET_PREFIX = "ws://3.89.90.31:8000/ws/users";// import.meta.env.VITE_WEBSOCKET_URL 
 
 export const AUTH_API = {
     login: `${URL_PREFIX}/auth/login`,
