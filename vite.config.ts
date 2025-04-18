5f8868383bf809676a9722a3d9da72a4154d6af9
doantrancaotri1108 <doantrancaotri1108@gmail.com>
2025-04-19 02:14:12 +0700

    allow internet

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+		allowedHosts: ['sa-ott-zalo.click', 'www.sa-ott-zalo.click']
+	}
+};
+
+export default config;
