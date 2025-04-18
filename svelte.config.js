d401b4075888126c44c9f55ae174430f2bd2bdb6
doantrancaotri1108 <doantrancaotri1108@gmail.com>
2025-04-19 02:06:50 +0700

    expose internet

--- svelte.config.js
+++ svelte.config.js
@@ -12,6 +12,12 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter()
+	},
+
+	server: {
+		host: '0.0.0.0',
+		port: 5173,
+		allowedHosts: ['sa-ott-zalo.click', 'www.sa-ott-zalo.click']
 	}
 };
 
